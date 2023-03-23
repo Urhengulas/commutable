7c0bb54e0a943a7ab2087c0ec3e336b1d77ada77
Urhengulas <johann.hemmann@code.berlin>
2023-03-23 16:50:23 +0100

    Calculate the distance with the gmaps API

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+# std
 from typing import Dict, Tuple
+
+# local
+from maps import measure_route
 
 """Map commute type to gramm of co2 per km and amount of people"""
 TRANSPORT_TYPES: Dict[str, Tuple[float, int]] = {
@@ -7,18 +11,6 @@ TRANSPORT_TYPES: Dict[str, Tuple[float, int]] = {
     "CAR": (118.0, 1),
     "CAR_POOL": (118.0, 2),
 }
-
-
-def calculate_route(commute_type: str) -> int:
-    """
-    Calculate the route.
-
-    Returns the distance in meter.
-    """
-    distance = 20_000
-    if commute_type == "CAR_POOL":
-        distance *= 1.1
-    return distance
 
 
 def calculate_emission(
@@ -33,13 +25,20 @@ def calculate_emission(
 
 
 if __name__ == "__main__":
-    transport_emissions: Dict[str, int] = dict()
-    for commute_type, (emission_factor, number_of_people) in TRANSPORT_TYPES.items():
-        distance = calculate_route(commute_type)
-        total_emission = calculate_emission(distance, emission_factor, number_of_people)
-        transport_emissions[commute_type] = total_emission
+    HOME = "Flutstraße 23, 12439 Berlin"
+    WORK = "Am Friedrichshain 20D, 10407 Berlin"
 
-    car_emissions = transport_emissions["CAR"]
-    for commute_type, total_emission in transport_emissions.items():
+    transport_emissions: Dict[str, Tuple[int, int]] = dict()
+    for commute_type, (emission_factor, number_of_people) in TRANSPORT_TYPES.items():
+        (distance, duration) = measure_route(HOME, WORK, commute_type)
+        total_emission = calculate_emission(distance, emission_factor, number_of_people)
+        transport_emissions[commute_type] = (total_emission, duration)
+
+    (car_emissions, _) = transport_emissions["CAR"]
+    for commute_type, (total_emission, duration) in transport_emissions.items():
         savings = 100 - (total_emission / car_emissions * 100)
-        print(f"{commute_type}: {total_emission} g CO2 ({savings:.2f}% reduction)")
+        minutes = int(duration / 60)
+        kg = int(total_emission / 1000)
+        print(
+            f"{commute_type} takes {minutes} min and produces {kg} kg of CO2. That is a {savings:.2f}% reduction compared to taking the car."
+        )
